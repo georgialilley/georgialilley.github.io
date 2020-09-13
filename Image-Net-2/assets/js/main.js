85590979ec7fef9473d67a91de15d0773a76116d
georgialilley <glilley99@gmail.com>
2020-09-13 22:41:57 +1000

    First draft changes

--- Image-Net-2/assets/js/main.js
+++ Image-Net-2/assets/js/main.js
@@ -2,7 +2,27 @@ $(document).ready(function(){
 	console.log('hello world');
 
 	$('.btn-about').click(function(){
-		$('.about').toggleClass('active');
+		$('.about').addClass('active');
 	});
+
+	$('.close').click(function(){
+		$('.about').addClass('hide');
+	});
+
+	$('.btn-cat').click(function(){
+		$('.nav').addClass('active');
+	});
+
+	$('.btn-return').click(function(){
+		$('.nav').addClass('return');
+	});
+
+	$('.btn-art').click(function(){
+		$('.artists').toggleClass('active');
+	});	
+
+	$('.btn-info').click(function(){
+		$('.info').toggleClass('active');
+	});		
 
 });
